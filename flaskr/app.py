cc00e331737bedb7521a46829d9750f8d310acf7
dkeum <dkeum@sfu.ca>
2024-01-04 01:39:10 -0800

    final testing

--- flaskr/app.py
+++ flaskr/app.py
@@ -22,6 +22,50 @@ def hello():
     response_data = {"message": "hi"}
     return jsonify(response_data), 200
 
+def predict_classes():
+
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return jsonify({'error': 'No file part'})
+
+        file = request.files['file']
+
+        # Convert the uploaded image to PIL format and resize it
+        image_pil = Image.open(file).convert('L')  # Convert image to grayscale
+        image_resized = image_pil.resize((28, 28))  # Resize image to 28x28
+        
+        # image_resized.show()
+
+        # Convert image to numpy array and normalize pixel values
+        image_array = np.array(image_resized) / 255.0
+
+        # Reshape the image to match the model's expected input shape
+        image_reshaped = image_array.reshape(1, 28, 28)
+
+        # turn ndarray into tensor
+        image_reshaped = torch.from_numpy(image_reshaped)
+
+        # change from double to float
+        image_reshaped = [image_reshaped.float()]
+
+        # Load your model (FashionMNISTModelV2) and make predictions
+        loaded_model = FashionMNISTModelV2(input_shape=1, hidden_units=10, output_shape=10)
+        MODEL_SAVE_PATH = 'flaskr/models/cv_model.pth'
+        loaded_state_dict = torch.load(MODEL_SAVE_PATH, map_location=torch.device('cpu'))
+        loaded_model.load_state_dict(loaded_state_dict)
+
+        # Make predictions
+        pred_probs = make_predictions(model=loaded_model, data=image_reshaped)
+        # print(pred_probs)
+        pred_classes = pred_probs.argmax(dim=1).numpy()[0]
+        # print(pred_classes)
+        labels = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+        prediction = labels[pred_classes]
+
+        response_data = {"prediction": str(prediction)}
+        return jsonify(response_data), 200
+
+
 
 # def create_app(test_config=None):
 #     # create and configure the app
